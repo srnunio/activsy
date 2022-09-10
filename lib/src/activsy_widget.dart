ce331f65410dfe9569f870328509cfc0e39065f2
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-09-10 13:13:19 +0100

    config and implement onEvent listener

--- lib/src/activsy_widget.dart
+++ lib/src/activsy_widget.dart
@@ -5,11 +5,20 @@ class ActivsyWidget extends StatelessWidget {
   /// [builder]  principal widget
   final Widget Function(BuildContext) builder;
 
+  final Function(PointerEvent)? onEvent;
+
   /// [detectedMouseAction] enable/disable mouse detector
   final bool detectedMouseAction;
 
-  ActivsyWidget({required this.builder, this.detectedMouseAction = false})
+  ActivsyWidget(
+      {required this.builder, this.detectedMouseAction = false, this.onEvent})
       : assert(Activsy.isInitialized, 'activsy no initialized');
+
+  /// notify when has event
+  void _onEvent(PointerEvent event) {
+    Activsy.reset();
+    if (onEvent != null) onEvent!(event);
+  }
 
   /// sets up the widget to be shown [_builder]
   Widget _builder(BuildContext context) {
@@ -17,9 +26,9 @@ class ActivsyWidget extends StatelessWidget {
     if (detectedMouseAction) {
       return MouseRegion(
         child: builder(context),
-        onEnter: (_) => Activsy.reset(),
-        onHover: (_) => Activsy.reset(),
-        onExit: (_) => Activsy.reset(),
+        onEnter: _onEvent,
+        onHover: _onEvent,
+        onExit: _onEvent,
       );
     }
     return builder(context);
@@ -30,11 +39,11 @@ class ActivsyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Listener(
         child: _builder(context),
-        onPointerDown: (_) => Activsy.reset(),
-        onPointerUp: (_) => Activsy.reset(),
-        onPointerSignal: (_) => Activsy.reset(),
-        onPointerMove: (_) => Activsy.reset(),
-        onPointerCancel: (_) => Activsy.reset(),
-        onPointerHover: (_) => Activsy.reset(),
+        onPointerDown: _onEvent,
+        onPointerUp: _onEvent,
+        onPointerSignal: _onEvent,
+        onPointerMove: _onEvent,
+        onPointerCancel: _onEvent,
+        onPointerHover: _onEvent,
       );
 }
